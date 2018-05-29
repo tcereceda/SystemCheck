a33e03f43af93e644fa9fd908faabcc50bce4afe
Teresa Cereceda <tcereceda@gmail.com>
2018-05-29 11:00:27 +0200

    Agregar archivos de proyecto.

---
+++ M101DotNet/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Bson.Serialization; // BsonClassMap
+using MongoDB.Bson.Serialization.Conventions;
+
+namespace M101DotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync(args).Wait();
+            Console.WriteLine("Press Enter");
+            Console.ReadLine();
+        }
+        static async Task MainAsync(string[] args)
+        {
+            var client = new MongoClient();
+            var db = client.GetDatabase("students");
+            var col = db.GetCollection<BsonDocument>("grades");
+
+      
+            var filter = new BsonDocument("type", "homework");
+
+            var list = await col.Find(filter)
+                .Sort("{student}")
+                .Sort("{score: 1}")
+                .ForEachAsync()
+
+            //foreach (var doc in list)
+            //{
+            //    Console.WriteLine(doc);
+            //}
+
+        }
+        private class Person
+        {
+            public ObjectId Id { get; set; }
+
+            public string Name { get; set; }
+
+            public int Age { get; set; }
+
+            public string Profesion { get; set; }
+
+        }
+    }
+}
---
+++ M101DotNet/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("M101DotNet")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("M101DotNet")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("fbf687f8-15ea-4cfb-a0e5-85b590e94109")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o utilizar los números de compilación y de revisión predeterminados
+// mediante el carácter '*', como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
